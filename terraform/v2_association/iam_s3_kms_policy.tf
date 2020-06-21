175f48b44ee5bc5a90aaffb1be678a5330c5f5f9
shahroz.haider <shahroz.haider@northbaysolutions.net>
2020-06-21 20:17:33 +0500

    v2 with tf v12 updates

---
+++ terraform/v2_association/iam_s3_kms_policy.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_policy" "datafence_lambda_role_policy_s3" {
+  name        = "devops-datafence-s3-put-object-policy"
+  path        = "/"
+  description = "IAM policy for putting object inside s3"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["s3:PutObject"],
+      "Resource": "${aws_s3_bucket.datafence_s3_bucket.arn}/*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "datafence_s3_policy_attachment" {
+  role       = aws_iam_role.datafence_lambda_role.name
+  policy_arn = aws_iam_policy.datafence_lambda_role_policy_s3.arn
+}
+
+
+resource "aws_iam_policy" "datafence_lambda_role_policy_kms" {
+  name        = "devops-datafence-kms-policy"
+  path        = "/"
+  description = "IAM policy for kms"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["kms:*"],
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "datafence_kms_policy_attachment" {
+  role       = aws_iam_role.datafence_lambda_role.name
+  policy_arn = aws_iam_policy.datafence_lambda_role_policy_kms.arn
+}
