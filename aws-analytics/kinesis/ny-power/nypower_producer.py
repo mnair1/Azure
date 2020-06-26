0b58ea963e8afc21bd87c428b8cb82be20da1510
Manoj Kukreja <mkukreja1@gmail.com>
2020-06-26 15:42:01 -0400

    Adding new example for S3

---
+++ aws-analytics/kinesis/ny-power/nypower_producer.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+from datetime import datetime
+import calendar
+import random
+import time
+
+stream_name = 'ny-power-fuel-hydro'
+
+kinesis_client = boto3.client('kinesis', region_name='us-east-1')
+
+def put_to_stream(city, temp, sensor_timestamp):
+    payload = {
+                'power': str(temp),
+                'timestamp': str(sensor_timestamp),
+                'city': city
+              }
+
+    print payload
+
+    put_response = kinesis_client.put_record(
+                        StreamName=stream_name,
+                        Data=json.dumps(payload),
+                        PartitionKey=city)
+
+while True:
+    temp = random.randint(100, 500)
+    sensor_timestamp = calendar.timegm(datetime.utcnow().timetuple())
+    locations = ['Accord','Albany', 'Beacon','Brighton','Calcium', 'Bethpage','Centerport','Depauville', 'Elmira','Galeville','Lockport', 'Oneida']
+    city = random.choice(locations)
+
+    put_to_stream(city, temp, sensor_timestamp)
+
+    # wait for 5 second
+    time.sleep(5)
