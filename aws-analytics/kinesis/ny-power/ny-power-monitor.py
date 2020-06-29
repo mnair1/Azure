b3510c9a52827b638375642b1fdaf8b982b8689c
Manoj Kukreja <mkukreja1@gmail.com>
2020-06-29 13:40:27 -0400

    Adding new example for S3

--- aws-analytics/kinesis/ny-power/ny-power-monitor.py
+++ aws-analytics/kinesis/ny-power/ny-power-monitor.py
@@ -11,8 +11,8 @@ ses = boto3.client('ses')
 email_address = 'XXXXXXXXXXXXX'
 
 def lambda_handler(event, context):
-    subject = 'High Power Usage Alert'
-    body_text = 'High Power Usage Alert for %s' %  (json.dumps(event['city']))
+    subject = 'High Power Usage Alert for ' + json.dumps(event['city'])
+    body_text =  json.dumps(event['city']) + ' is currently running at ' + json.dumps(event['power']) + ' MW'
     ses.send_email(Source=email_address,
                    Destination={'ToAddresses': [email_address]},
                    Message={'Subject': {'Data': subject}, 'Body': {'Text': {'Data': body_text}}})
--- aws-analytics/kinesis/ny-power/nypower_producer.py
+++ aws-analytics/kinesis/ny-power/nypower_producer.py
@@ -16,7 +16,7 @@ def put_to_stream(city, temp, sensor_timestamp):
                 'city': city
               }
 
-    print payload
+    print json.dumps(payload)
 
     put_response = kinesis_client.put_record(
                         StreamName=stream_name,
