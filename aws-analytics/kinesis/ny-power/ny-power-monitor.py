898d09fc8bd621e505ba5a4d990417f9a7b6c89e
Manoj Kukreja <mkukreja1@gmail.com>
2020-06-29 15:48:40 -0400

    Adding new example for S3

--- aws-analytics/kinesis/ny-power/ny-power-monitor.py
+++ aws-analytics/kinesis/ny-power/ny-power-monitor.py
@@ -3,6 +3,7 @@ from __future__ import division
 import boto3
 import json
 import logging
+import base64
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -11,13 +12,21 @@ ses = boto3.client('ses')
 email_address = 'XXXXXXXXXXXXX'
 
 def lambda_handler(event, context):
-    subject = 'High Power Usage Alert for ' + json.dumps(event['city'])
-    body_text =  json.dumps(event['city']) + ' is currently running at ' + json.dumps(event['power']) + ' MW'
-    ses.send_email(Source=email_address,
+    for record in event['records']:
+        #print(record['recordId'])
+        payload = base64.b64decode(record['data'])  
+
+        dict_payload=json.loads(payload)
+        #print(dict_payload['CITY'])
+        
+        subject = 'High Power Usage Alert for ' + dict_payload['CITY']
+        body_text =  dict_payload['CITY'] + ' is currently running at ' + str(dict_payload['COL_POWER']) + ' MW'
+        ses.send_email(Source=email_address,
                    Destination={'ToAddresses': [email_address]},
                    Message={'Subject': {'Data': subject}, 'Body': {'Text': {'Data': body_text}}})
-    logger.info('Email has been sent')
-    return {
-        'statusCode': 200,
-        'body': json.dumps('High Power Usage Alert Generated.')
-    }
+        logger.info('Email has been sent')
+        return {
+                'statusCode': 200,
+                'body': json.dumps('High Power Usage Alert Generated.')
+             }
+   
