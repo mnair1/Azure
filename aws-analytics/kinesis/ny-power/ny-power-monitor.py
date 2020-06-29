5d2185752e756cc053a49269dc203a1766636082
Manoj Kukreja <mkukreja1@gmail.com>
2020-06-29 11:35:56 -0400

    Adding new example for S3

--- aws-analytics/kinesis/ny-power/ny-power-monitor.py
+++ aws-analytics/kinesis/ny-power/ny-power-monitor.py
@@ -12,12 +12,12 @@ email_address = 'XXXXXXXXXXXXX'
 
 def lambda_handler(event, context):
     subject = 'New File uploaded to S3'
-    body_text = 'A new file has been uploaded to S3. Here are the details: %s' %  (json.dumps(event))
+    body_text = 'High Power Usage Alert for %s' %  (json.dumps(event['city']))
     ses.send_email(Source=email_address,
                    Destination={'ToAddresses': [email_address]},
                    Message={'Subject': {'Data': subject}, 'Body': {'Text': {'Data': body_text}}})
     logger.info('Email has been sent')
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('High Power Usage Alert Generated.')
     }
