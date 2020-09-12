04fd53da5c6f39c935faff16d65eaaebf4659992
shahroz.haider <shahroz.haider@northbaysolutions.net>
2020-09-12 06:34:19 +0500

    deploy 6

--- codepipeline/user_data.sh
+++ codepipeline/user_data.sh
@@ -7,3 +7,10 @@ pip3 install flask_cors==3.0.7
 apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt -y install nodejs
 apt install -y mongodb
+
+apt install -y ruby
+apt install -y awscli
+cd /home/ubuntu
+aws s3 cp s3://aws-codedeploy-us-east-2/latest/install . --region us-east-2
+chmod +x ./install
+./install auto
