26753f78f6d12be12060f7de57022acedfdca0be
shahroz.haider <shahroz.haider@northbaysolutions.net>
2020-09-12 17:13:44 +0500

    deploy 14

--- codepipeline/user_data.sh
+++ codepipeline/user_data.sh
@@ -7,7 +7,6 @@ pip3 install flask_cors==3.0.7
 apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt -y install nodejs
 apt install -y mongodb
-
 apt install -y ruby
 apt install -y awscli
 cd /home/ubuntu
