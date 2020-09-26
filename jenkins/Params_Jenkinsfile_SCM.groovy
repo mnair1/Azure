12df958b5ce1ab9aed3cfd068ca0cc08643356be
shahroz.haider <shahroz.haider@northbaysolutions.net>
2020-09-26 07:13:53 +0500

    jenkins testing 32

--- jenkins/Params_Jenkinsfile_SCM.groovy
+++ jenkins/Params_Jenkinsfile_SCM.groovy
@@ -32,30 +32,52 @@ pipeline {
         stage('Pre-Build') {
             
             steps {
-                echo "list the directory"
-                sh 'ls'
-                // sh 'cd codepipeline/multi-tier-app'
-                echo "list multi tier app directory"
-                sh 'ls $MULTI_TIER_APP_DIRECTORY'
+                dir("$MULTI_TIER_APP_DIRECTORY") {
+                    echo "list the directory"
+                    echo "list multi tier app directory"
+                    sh 'ls'
 
-                echo "Existing Python code"
-                sh 'cat $MULTI_TIER_APP_DIRECTORY/mongo.py'
-                echo "Updating Python Code with Specs"
-                sh "sed -i 's/<MONGO_DB>/$MONGO_DB/g' $MULTI_TIER_APP_DIRECTORY/mongo.py"
-                sh "sed -i 's/<MONGO_HOST>/$MONGO_HOST/g' $MULTI_TIER_APP_DIRECTORY/mongo.py"
-                sh "sed -i 's/<MONGO_PORT>/$MONGO_PORT/g' $MULTI_TIER_APP_DIRECTORY/mongo.py"
-                //sh "cat mongo.py"
+                    echo "Updating Python Code with Specs"
+                    sh "sed -i 's/<MONGO_DB>/$MONGO_DB/g' mongo.py"
+                    sh "sed -i 's/<MONGO_HOST>/$MONGO_HOST/g' mongo.py"
+                    sh "sed -i 's/<MONGO_PORT>/$MONGO_PORT/g' mongo.py"
+                    
+                    echo "Updating NodeJs config with Specs"
+                    sh "sed -i 's/<VERSION_AXIOS>/$VERSION_AXIOS/g' package.json"
+                    sh "sed -i 's/<VERSION_REACT>/$VERSION_REACT/g' package.json"
+                    sh "sed -i 's/<VERSION_REACT_DOM>/$VERSION_REACT_DOM/g' package.json"
+                    sh "sed -i 's/<VERSION_REACT_SCRIPTS>/$VERSION_REACT_SCRIPTS/g' package.json"
+                    sh "sed -i 's/<FLASK_HOST>/$FLASK_HOST/g' package.json"
+                    sh "sed -i 's/<FLASK_PORT>/$FLASK_PORT/g' package.json"
+                    
+                    sh "cat package.json"
+                    sh "cat mongo.py"
+                }
 
-                echo "Existing Package.json"
-                sh "cat $MULTI_TIER_APP_DIRECTORY/package.json"
-                echo "Updating NodeJs config with Specs"
-                sh "sed -i 's/<VERSION_AXIOS>/$VERSION_AXIOS/g' $MULTI_TIER_APP_DIRECTORY/package.json"
-                sh "sed -i 's/<VERSION_REACT>/$VERSION_REACT/g' $MULTI_TIER_APP_DIRECTORY/package.json"
-                sh "sed -i 's/<VERSION_REACT_DOM>/$VERSION_REACT_DOM/g' $MULTI_TIER_APP_DIRECTORY/package.json"
-                sh "sed -i 's/<VERSION_REACT_SCRIPTS>/$VERSION_REACT_SCRIPTS/g' $MULTI_TIER_APP_DIRECTORY/package.json"
-                sh "sed -i 's/<FLASK_HOST>/$FLASK_HOST/g' $MULTI_TIER_APP_DIRECTORY/package.json"
-                sh "sed -i 's/<FLASK_PORT>/$FLASK_PORT/g' $MULTI_TIER_APP_DIRECTORY/package.json"
-                sh "cat $MULTI_TIER_APP_DIRECTORY/package.json"
+                // echo "list the directory"
+                // sh 'ls'
+                // // sh 'cd codepipeline/multi-tier-app'
+                // echo "list multi tier app directory"
+                // sh 'ls $MULTI_TIER_APP_DIRECTORY'
+
+                // echo "Existing Python code"
+                // sh 'cat $MULTI_TIER_APP_DIRECTORY/mongo.py'
+                // echo "Updating Python Code with Specs"
+                // sh "sed -i 's/<MONGO_DB>/$MONGO_DB/g' $MULTI_TIER_APP_DIRECTORY/mongo.py"
+                // sh "sed -i 's/<MONGO_HOST>/$MONGO_HOST/g' $MULTI_TIER_APP_DIRECTORY/mongo.py"
+                // sh "sed -i 's/<MONGO_PORT>/$MONGO_PORT/g' $MULTI_TIER_APP_DIRECTORY/mongo.py"
+                // //sh "cat mongo.py"
+
+                // echo "Existing Package.json"
+                // sh "cat $MULTI_TIER_APP_DIRECTORY/package.json"
+                // echo "Updating NodeJs config with Specs"
+                // sh "sed -i 's/<VERSION_AXIOS>/$VERSION_AXIOS/g' $MULTI_TIER_APP_DIRECTORY/package.json"
+                // sh "sed -i 's/<VERSION_REACT>/$VERSION_REACT/g' $MULTI_TIER_APP_DIRECTORY/package.json"
+                // sh "sed -i 's/<VERSION_REACT_DOM>/$VERSION_REACT_DOM/g' $MULTI_TIER_APP_DIRECTORY/package.json"
+                // sh "sed -i 's/<VERSION_REACT_SCRIPTS>/$VERSION_REACT_SCRIPTS/g' $MULTI_TIER_APP_DIRECTORY/package.json"
+                // sh "sed -i 's/<FLASK_HOST>/$FLASK_HOST/g' $MULTI_TIER_APP_DIRECTORY/package.json"
+                // sh "sed -i 's/<FLASK_PORT>/$FLASK_PORT/g' $MULTI_TIER_APP_DIRECTORY/package.json"
+                // sh "cat $MULTI_TIER_APP_DIRECTORY/package.json"
             }
         }
         stage('Build') {
@@ -67,19 +89,27 @@ pipeline {
                 }
             }
             steps {
-                echo "${JOB_NAME}"
-                sh "pwd"
-                sh "ls"
+                // echo "${JOB_NAME}"
+                // sh "pwd"
+                // sh "ls"
                 sh "ls /datafence"
-                sh "cat /datafence/jenkins/multi-tier-app/package.json"
-                echo "Node Version"
-                sh 'node --version'
-                sh 'npm --version'
+                dir("/datafence/$MULTI_TIER_APP_DIRECTORY") {
+                    
+                    sh "cat package.json"
+                    echo "Node Version"
+                    sh 'npm --version'
+                    sh 'npm install'
+                    
+                }
+                // sh "cat /datafence/jenkins/multi-tier-app/package.json"
+                // echo "Node Version"
+                // sh 'node --version'
+                // sh 'npm --version'
                 
                 
-                echo "Build Started at `date`"
-                sh 'cd /datafence/jenkins/multi-tier-app/ && npm install'
-                echo "Build Completed at `date`"
+                // echo "Build Started at `date`"
+                // sh 'cd /datafence/jenkins/multi-tier-app/ && npm install'
+                // echo "Build Completed at `date`"
 
                 
             }
@@ -104,8 +134,16 @@ pipeline {
                 // sh 'cat $MULTI_TIER_APP_DIRECTORY/mongo.py'
                 // sh 'cat $MULTI_TIER_APP_DIRECTORY/package.json'
                 
+                sh "mkdir artifacts_folder"
                 
-                // sh 'cp $MULTI_TIER_APP_DIRECTORY/app-artifact.zip .'
+                sh 'cp $JENKINS_DIRECTORY/deploy/appspec.yml artifacts_folder/'
+                sh 'cp $JENKINS_DIRECTORY/deploy/uncompress artifacts_folder/'
+                sh 'cp $JENKINS_DIRECTORY/deploy/start_flask artifacts_folder/'
+                sh 'cp $JENKINS_DIRECTORY/deploy/start_node artifacts_folder/'
+                sh 'cp $MULTI_TIER_APP_DIRECTORY/app-artifact.zip artifacts_folder/'
+                
+                sh 'ls build_artifacts'
+                
                 // sh 'cp $JENKINS_DIRECTORY/deploy/appspec.yml .'
                 // sh 'cp $JENKINS_DIRECTORY/deploy/uncompress .'
                 // sh 'cp $JENKINS_DIRECTORY/deploy/start_flask .'
@@ -118,7 +156,7 @@ pipeline {
                 // sh 'cp uncompress build_artifacts/'
                 // sh 'cp start_flask build_artifacts/'
                 // sh 'cp start_node build_artifacts/'
-                // sh 'ls build_artifacts'
+                
             }
         }
         // stage('Build') {
